f8babbef2ed86efb89fd73fda35e450ba5799196
Rhythem Sharma <Rid.Sharma@utexas.edu>
2017-10-01 23:03:53 -0500

    Last update to Homework2

--- Homework/2/script_full_of_errors.m
+++ Homework/2/script_full_of_errors.m
@@ -1,41 +1,47 @@
-a = 2; % 1a is an invalid syntax form in matlab. Variables always start with letters, not numbers. 
+a = 2; 
+%{
+1a is an invalid syntax form in matlab. 
+Variables always start with letters, not numbers. 
+%}
 a1 = 6;
 %{
 B is an undefined variable which cannot give a1 a value.
 %}
 x = 2;
-y = x + 4; % is it 6?
+y = x + 4; % is it 6? - no because we were using an uppercase x
 %{
-Matlab is cased sensitive and therefore x != X. They are two separate
+Matlab is cased sensitive and therefore x isn't the same as X. They are two separate
 variables.
 %}
 pi = 4 * atan(1);
 %{
 The expression pi = 4 *; atan(1); is incomplete. The first ; marks the
-first end of the code, however it is incomplete since there is nothing
-being multiplied with 4.
+first end of the line, however it is incomplete since 4 isn't being multiplied with anything.
 %}
 disp(pi)
-pi = "3.14159";
+pi = '3.14159';
 %{
-The string isn't terminated properly. At the front of the string there are
-double quotes, however, at the end of the string there are single quotes.
+The initalization of the characters aren't terminated properly. At the front
+of the characters there are double quotes, however, at the end of the characters there are single quotes.
 %}
-pi = 3.14159;
-disp(tan(pi));
+disp(tan(str2double(pi)));
 %{
 The arguments for the function tan(x) requires that x is a double. Since
-pi is defined as a string, we get a syntactic error.
+pi is defined as a character, the tan function cannot execute properly.
 %}
 c = 4^3^2^3;
 e = ((c-78564)/c + 32);
 %{
 The input character is not valid. The variable here is a whitespace, which
-goes against the syntax for naming a variable in Matlab. There are also an
+improper syntax for naming a variable in Matlab. There is also an
 unbalanced number of parenthesis in the expression (one extra at the end).
 %}
 year = 2017;
-disp(['The year is ' year]); % does it display 2017 correctly?
+disp(['The year is ' num2str(year)]); % does it display 2017 correctly? -no returns the ascii value of 2017
+%{
+In order for the number to print as a string without changing to its ascii
+value, we add a matlab defined function num2str(<number>).
+%}
 stuff = {'a' 'b' 4 21 'c'};
 beginning = stuff(1);
 %{
@@ -43,20 +49,20 @@ The array index starts from 1, not 0
 %}
 last = stuff(5);
 %{
-End is a keyword in matlab and therefore is reserved for other purposes.
+End is a keyword in matlab and therefore is reserved for another purpose.
 It cannot be used as a variable name.
 %}
 discount = .12;
 %{
 The % symbol comments information after it. Therefore, the semi-colon is
 commented and gives us a warning that in order to prevent the interperator
-from stating the value of discount in the command window, there needs to be
+from printing the value of discount in the command window, there needs to be
 a semi-colon.
 %}
 AMOUNT = 120.;
 %{
-The expression is incomplete. There is nothing after the subtraction symbol
-and therefore 120 is not being subtracted by anything.
+The expression is incomplete. There needs to be a number on either side 
+of the subtraction symbol, however, 120 is not being subtracted by anything.
 %}
 amount = 120;
 %{
@@ -81,9 +87,9 @@ The value of fox is defined as false, but we are using the equality symbol
 which instead compares the two values. 
 %}
 wolf = fox == true;
-Persian = ['Persian' ' is ' 'a' ' human ' 'language'];
-Spanish = {'Spanish ' 'is ' ' another'  'language'};
+Persian = ['Persian' ' is ' 'a' ' human ' 'language']; %array
+Spanish = {'Spanish ' 'is ' ' another'  'language'}; %cell array
 %{
 There is an unbalanced number of open parenthesis and close parenthesis.
 %}
-disp([Persian(1:7) 'is not the same as' Spanish(1:1)]);
+disp([Persian(1:7) ' is not the same as ' Spanish{1}]);
