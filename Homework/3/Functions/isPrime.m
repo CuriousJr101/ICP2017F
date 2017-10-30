f25421123662e7f76f8f2c295cddb5b69335bd52
Rhythem Sharma <Rid.Sharma@utexas.edu>
2017-10-29 22:25:00 -0500

    Homework 3 Function isPrime() edit

--- Homework/3/Functions/isPrime.m
+++ Homework/3/Functions/isPrime.m
@@ -3,13 +3,13 @@ function bool = isPrime(n)
     sq = floor(sqrt(n)); %tells us the number of cases that we need to check
     bool = test(n, sq);
     function prime = test(n, sq)
+        prime = true;
         if(n == 1)
             prime = false;
         elseif(n == 0)
             prime = '0 is a special number that is neither prime nor composite';
         elseif(sq > 1 && mod(n, sq))
-            prime = true;
-            test(n, (sq - 1));
+            prime = test(n, (sq - 1));
         elseif(sq > 1 && mod(n, sq) == 0) %if the number can be divided by another number, then it isn't prime
             prime = false;
         elseif(sq == 1) %this notes that 2 and 3 are prime (their mod is 1)
